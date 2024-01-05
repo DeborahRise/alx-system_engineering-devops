bbca3cf8e4c62aeb066cc9a784dc3cd1de0ed781
DeborahRise <debie.rise.bookings@gmail.com>
2024-01-05 00:38:24 +0000

    textme app

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,4 +2,4 @@
 # A Ruby script that accepts one argument
 # And pass it to a regular expression matching method
 
-puts ARGV[0].scan(/\[from:\+?\d{7,15}\]\s\[to:\+?\d{7,15}\]\s\[flags:[\w._%-:]{3,15}\]/).join(',')
+puts ARGV[0].scan(/\[from:[\w.+]{7,16}\]\s\[to:[\w.+]{7,16}\]\s\[flags:[\w._%-:]{3,15}\]/).join(',')
