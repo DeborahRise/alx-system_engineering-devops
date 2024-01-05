708254542608d7116902ea8baba3541450b175c5
DeborahRise <debie.rise.bookings@gmail.com>
2024-01-05 00:26:38 +0000

    textme app

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+# A Ruby script that accepts one argument
+# And pass it to a regular expression matching method
+
+puts ARGV[0].scan(/\[from:\+?\d{7,15}\]\s\[to:\+?\d{7,15}\]\s\[flags:[\w._%-:]{3,15}\]/).join
