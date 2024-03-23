f9a3c485969bfed2ab7f8ecc38872aa99ad626ad
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-23 04:03:40 +0000

    thank you task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# How to Create a file using puppet
+
+  file { '/tmp/school':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
