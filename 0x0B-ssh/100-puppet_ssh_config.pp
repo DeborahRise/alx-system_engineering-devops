4c3847c31404961ffa36b43daf710b645b81c9e2
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-26 06:34:23 +0000

    advanced task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,21 +1,13 @@
 #  set up your client SSH configuration file
-
-file { ~/.ssh/school:
+file { '/etc/ssh/ssh_config':
 ensure => 'present',
-enable => True,
-content => '~/.ssh/school'
-}
-
-
-file { ~/.ssh/config:
 path => /etc/ssh/ssh_config,
-ensure => 'present',
-mode => '0744'
 content => "
 Host Alx,
 HostName 54.82.172.244,
-User 'ubuntu',
-IdentityFile ~/.ssh/school
-PasswordAuthentication no
+User 'ubuntu'
 ",
+line =>IdentityFile ~/.ssh/school,
+line => PasswordAuthentication no,
+
 }
