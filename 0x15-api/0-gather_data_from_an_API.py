b42e97cb5e8717644410a74576aeb7325cda2982
DeborahRise <debie.rise.bookings@gmail.com>
2024-04-22 19:03:24 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 """ Write a Python script that, uses REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
-
 import requests
 import sys
 import json
@@ -13,23 +11,25 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script.py <employee_id>")
         sys.exit(1)
-    
+
     user_id = sys.argv[1]
-    url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id)
-    
+    url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+        user_id)
+
     # Make a GET request to the API
     response = requests.get(url)
-    
+
     # Check if the request was successful
     if response.status_code == 200:
         tasks = response.json()
         total_tasks = len(tasks)
-        
+
         # Count completed tasks
         completed_tasks = sum(1 for task in tasks if task['completed'])
-        
-        print("Employee {} is done with tasks ({}/{})".format(user_id, completed_tasks, total_tasks))
-        
+
+        print("Employee {} is done with tasks ({}/{})"
+              .format(user_id, completed_tasks, total_tasks))
+
         # Print titles of completed tasks
         for task in tasks:
             if task['completed']:
