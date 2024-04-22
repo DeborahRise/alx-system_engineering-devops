f8a33cb007d7858f42a3f7838dfa3e99a56688d7
DeborahRise <debie.rise.bookings@gmail.com>
2024-04-22 18:53:01 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,27 +2,6 @@
 
 """ Write a Python script that, uses REST API, for a given employee ID,
 returns information about his/her TODO list progress.
-Requirements:
-You must use urllib or requests module
-The script must accept an integer as a parameter, which is the employee ID
-The script must display on the standard output of
-the employee TODO list progress in this exact format:
-First line: Employee EMPLOYEE_NAME is done with
-tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
-EMPLOYEE_NAME: name of the employee
-NUMBER_OF_DONE_TASKS: number of completed tasks
-TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum of
-completed and non-completed tasks
-
-Second and N next lines display the title of completed tasks:
-TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
-employee_id = sys.argv[1]
-
-    response = requests.get('https://jsonplaceholder.typicode.com/todos/{}'.format(employee_id))
-
-    user = response.json()
-
-    print("Employee {} is done with tasks({}/{})".format(user['name'], user['completed'], user['total_tasks']))
 """
 
 import requests
@@ -30,16 +9,28 @@ import sys
 import json
 
 if __name__ == "__main__":
+    # Check if an employee ID is provided as a command-line argument
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+    
     user_id = sys.argv[1]
-    url = ('https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id))
+    url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id)
+    
+    # Make a GET request to the API
     response = requests.get(url)
-    user = response.json
-    name = user.get('name')
-    total_tasks = len(user.get('title'))
-    complete_task = [ tasks for task in (user.get('title')), if task.get("completed") ]
-    completed_task = len(complete_task)
-
-    print("Employee {} is done with tasks({}/{})".format(name, completed_task, total_tasks))
-    for task in (user.get('title')):
-        if task.get("completed"):
-            print("\t{}".format(task.get("title")))
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        tasks = response.json()
+        total_tasks = len(tasks)
+        
+        # Count completed tasks
+        completed_tasks = sum(1 for task in tasks if task['completed'])
+        
+        print("Employee {} is done with tasks ({}/{})".format(user_id, completed_tasks, total_tasks))
+        
+        # Print titles of completed tasks
+        for task in tasks:
+            if task['completed']:
+                print("\t{}".format(task['title']))
