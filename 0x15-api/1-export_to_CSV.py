ae8a896b64d2704cd09528fab26043d148fcdb99
DeborahRise <debie.rise.bookings@gmail.com>
2024-04-24 22:31:16 +0100

    fornatted

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,18 +2,40 @@
 """ Write a Python script that, uses REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
-import csv
-import requests as r
+import requests
 import sys
+import csv
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    usr = r.get(url + "users/{}".format(user_id)).json()
-    username = usr.get("username")
-    to_do = r.get(url + "todos", params={"userId": user_id}).json()
+    url = 'https://jsonplaceholder.typicode.com/'
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow([user_id, username, elm.get("completed"),
-                          elm.get("title")]) for elm in to_do]
+    # Make a GET request to the API
+    response = requests.get(url + "users/{}".format(user_id))
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        user = response.json()
+        params = {"userId": user_id}
+        todos_response = requests.get(url + "todos", params=params)
+        tasks = todos_response.json()
+        total_tasks = len(tasks)
+
+        # Count completed tasks
+        completed_tasks = sum(1 for task in tasks if task['completed'])
+
+        """ print("Employee {} is done with tasks ({}/{})"
+              .format(user['username'], completed_tasks, total_tasks))
+
+        # Print titles of completed tasks
+        for task in tasks:
+            if task['completed']:
+                print("\t{}".format(task['title'])) """
+
+        csv_file = "{}.csv".format(user_id)
+
+        with open(csv_file, "w", newline="") as csvfile:
+            csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+            for task in tasks:
+                csv_writer.writerow([user_id, user['username'],
+                                     task['completed'], task['title']])
