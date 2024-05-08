478f1b5482f64f5b8d97160fea569e1b40b8986d
DeborahRise <debie.rise.bookings@gmail.com>
2024-05-08 06:10:15 +0100

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
 """
-recurse request
+Using reddit's API
 """
+import requests
+after = None
 
-import requests as r
 
-
-def recurse(subreddit, hot_list=[], after=""):
-    """
-    recurse request
-    """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+def recurse(subreddit, hot_list=[]):
+    """returning top ten post titles recursively"""
+    global after
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
             AppleWebKit/537.36 \
         (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0"
     }
-    param = {
-        "after": after,
-        "limit": 100,
-    }
-    response = r.get(url, headers=headers, params=param, allow_redirects=False)
-    if response.status_code == 404:
-        return None
-    else:
-        posts = response.json().get("data").get("children")
-        hot_list += [post.get("data").get("title") for post in posts]
-        after = response.json().get("data").get("after")
-        if after is not None:
-            recurse(subreddit, hot_list, after)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=headers,
+                           allow_redirects=False)
+
+    if results.status_code == 200:
+        after_data = results.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
+            recurse(subreddit, hot_list)
+        all_titles = results.json().get("data").get("children")
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
         return hot_list
+    else:
+        return (None)
