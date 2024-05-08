476ccc039c7756e9401b475bbe87c212bd9edcee
DeborahRise <debie.rise.bookings@gmail.com>
2024-05-08 05:53:21 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,14 +10,21 @@ def number_of_subscribers(subreddit):
     """
     queries the Reddit API and returns the number of subscribers
     """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
             AppleWebKit/537.36 \
         (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0"
     }
-    response = r.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    response = r.get(url, headers=headers)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
