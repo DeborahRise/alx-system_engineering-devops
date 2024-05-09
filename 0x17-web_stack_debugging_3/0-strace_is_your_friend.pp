54a2253838b29f97acdaf6ab8ddc9d8a2a86b106
DeborahRise <debie.rise.bookings@gmail.com>
2024-05-09 05:55:54 +0100

    web debug 3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,10 @@
+# Replace a line in a file on a server  with A puppet manuscript
+
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+#replace line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
+}
