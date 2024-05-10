a666f431137bf89f691717bf79e627a0b95bdab2
DeborahRise <debie.rise.bookings@gmail.com>
2024-05-10 05:26:07 +0100

    fixed mode of retrieveing data2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 """
-a function that queries the Reddit API and returns the number of subscribers
-for a given subreddit
+number of subscribers for a given subreddit
 """
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
     """
-    returns the number of total subscribers for a given subreddit.
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
     """
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-            AppleWebKit/537.36 (KHTML, like Gecko) \
-                Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0'
-    }
-    if not subreddit:
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
-    resp = requests.get(url, headers=headers)
-    if resp.status_code == 200:
-        subs = resp.json()['data']['subscribers']
-        return(subs)
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0
