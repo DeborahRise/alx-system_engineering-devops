46f98f78b883be7f746076a4cbbd437e8a4df437
DeborahRise <debie.rise.bookings@gmail.com>
2024-05-10 05:13:48 +0100

    fixed mode of retrieveing data

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
 a function that queries the Reddit API and returns the number of subscribers
+for a given subreddit
 """
 
 import requests
@@ -12,15 +13,16 @@ def number_of_subscribers(subreddit):
     """
 
     headers = {
-        'User_Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
             AppleWebKit/537.36 (KHTML, like Gecko) \
                 Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0'
     }
     if not subreddit:
         return 0
-    url = 'https://www.reddit.com/r/{subreddit}'
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
     resp = requests.get(url, headers=headers)
-    subs = resp.json()['subcribers']
-
-    return subs
+    if resp.status_code == 200:
+        subs = resp.json()['data']['subscribers']
+        return(subs)
+    
