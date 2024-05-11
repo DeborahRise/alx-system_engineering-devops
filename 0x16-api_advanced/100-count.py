f4245d8a89ae52a02a31f808095b69240d50f9d4
DeborahRise <debie.rise.bookings@gmail.com>
2024-05-11 04:11:54 +0100

    saving changes

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-""" raddit api"""
+"""
+Write a recursive function that queries the Reddit API,
+parses the title of all hot articles, and prints a sorted count
+of given keywords (case-insensitive, delimited by spaces.
+Javascript should count as javascript, but java should not).
+"""
 
 import json
 import requests
