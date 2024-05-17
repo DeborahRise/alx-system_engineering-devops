d1df5bad43f2345a18aaf1198f3021e119134bcc
DeborahRise <debie.rise.bookings@gmail.com>
2024-05-17 05:48:09 +0100

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -4,3 +4,9 @@ exec { 'fix_limit_hbton_user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
+
+# a cmd to increase the users file limit
+exec { 'increase_soft_file':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
