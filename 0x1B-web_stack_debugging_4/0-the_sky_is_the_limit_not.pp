f286207ba01666a1f07bd70badf759d8594f2981
DeborahRise <debie.rise.bookings@gmail.com>
2024-05-17 05:38:17 +0100

    0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# Task 0 to fix limit at nginx
+
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+   path    => '/usr/local/bin/:/bin/'
+}
+
+# always restart nginx
+-> exec { 'nginx-restart':
+    command => 'nginx restart',
+    path    => '/etc/init.d/'
+}
